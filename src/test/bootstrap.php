db643dd6e47873e2090774b43843fc94b73d16c7
Thomas Günther <thomas.guenther@tomsquared.de>
2021-05-05 17:54:26 +0200

    phpunit integration

--- src/test/bootstrap.php
+++ src/test/bootstrap.php
@@ -2,22 +2,22 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-$testPath = realpath(dirname(__FILE__));
-$appPath = realpath($testPath . '/../app');
-$vendorPath = realpath($testPath . '/../vendor');
-$varPath = realpath($testPath . '/../var');
+// $testPath = realpath(dirname(__FILE__));
+// $appPath = realpath($testPath . '/../app');
+// $vendorPath = realpath($testPath . '/../vendor');
+// $varPath = realpath($testPath . '/../var');
 
-set_include_path(implode(PATH_SEPARATOR, array($appPath, $vendorPath, $testPath, get_include_path())));
+// set_include_path(implode(PATH_SEPARATOR, array($appPath, $vendorPath, $testPath, get_include_path())));
 
 define('N2N_STAGE', 'test');
 require __DIR__ . '/../vendor/autoload.php';
 
-n2n\core\TypeLoader::register(true,
-		require __DIR__ . '/../vendor/composer/autoload_psr4.php',
-		require __DIR__ . '/../vendor/composer/autoload_classmap.php');
+// n2n\core\TypeLoader::register(true,
+// 		require __DIR__ . '/../vendor/composer/autoload_psr4.php',
+// 		require __DIR__ . '/../vendor/composer/autoload_classmap.php');
 
-n2n\core\N2N::initialize('', $varPath, new n2n\core\FileN2nCache());
+// n2n\core\N2N::initialize('', $varPath, new n2n\core\FileN2nCache());
 
-function test($value) {
-	var_dump($value);
-}
+// function test($value) {
+// 	var_dump($value);
+// }
